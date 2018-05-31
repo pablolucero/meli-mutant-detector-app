160ea99dba052a12207d3b97e0f7ec8d1c256e17
Pablo Lucero <plucero87@gmail.com>
2018-05-30 21:38:11 -0300

    DnaResultRepository - replace crud repo with jpa

--- src/main/java/com/meli/mutantdetector/repository/DnaResultRepository.java
+++ src/main/java/com/meli/mutantdetector/repository/DnaResultRepository.java
@@ -1,9 +1,9 @@
 package com.meli.mutantdetector.repository;
 
 import com.meli.mutantdetector.model.DnaResult;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DnaResultRepository extends CrudRepository<DnaResult, Long> {
+public interface DnaResultRepository extends JpaRepository<DnaResult, Long> {
 
     boolean existsByDna(String dna);
 
