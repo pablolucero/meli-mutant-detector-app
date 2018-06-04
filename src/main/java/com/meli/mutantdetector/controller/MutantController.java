8b82a489ba66137995c78fcc97c64062661eb449
Pablo Lucero <plucero87@gmail.com>
2018-06-04 12:11:14 -0300

    add better logging in MutantController

--- src/main/java/com/meli/mutantdetector/controller/MutantController.java
+++ src/main/java/com/meli/mutantdetector/controller/MutantController.java
@@ -2,6 +2,8 @@ package com.meli.mutantdetector.controller;
 
 import com.meli.mutantdetector.dto.DnaDTO;
 import com.meli.mutantdetector.service.MutantDetectorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MutantController {
 
+    private static final Logger log = LoggerFactory.getLogger(MutantController.class);
+
     private MutantDetectorService mutantDetector;
 
     @Autowired
@@ -23,11 +27,16 @@ public class MutantController {
     @RequestMapping(value = "/mutant", method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity<String> isMutant(@RequestBody DnaDTO dna) {
 
-        System.out.println(dna);
+        log.info(String.format("Checking DNA: %s", dna.getDna()));
 
         try {
-            if (mutantDetector.isMutant(dna.getDna())) return ResponseEntity.ok("OK");
-            else return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            if (mutantDetector.isMutant(dna.getDna())) {
+                log.info(String.format("DNA: %s is mutant", dna.getDna()));
+                return ResponseEntity.ok("OK");
+            } else {
+                log.info(String.format("DNA: %s is NOT mutant", dna.getDna()));
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
